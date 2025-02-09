7fcccaf866d96d96dfc5c366151d61b1ff738e3e
millenialprophet007 <166893817+millenialprophet007@users.noreply.github.com>
2025-02-09 11:11:07 +0530

    Update main.tf  with the out for id's created

--- main.tf
+++ main.tf
@@ -11,3 +11,11 @@ resource "local_file" "samplefile2" {
   filename = "samplefile2.txt"
   content = "This is the second sample file created"
 }
+
+output "samplefile_id" {
+  value = local_file.samplefile.id
+}
+
+output "samplefile2_id" {
+  value = local_file.samplefile2.id
+}
