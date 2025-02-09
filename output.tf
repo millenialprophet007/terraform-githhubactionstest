27a70f311651ae6fac7130025fcf817bb51c1ad2
millenialprophet007 <166893817+millenialprophet007@users.noreply.github.com>
2025-02-09 11:27:47 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,15 @@
+output "samplefile_id" {
+  value = local_file.samplefile.id
+}
+
+output "samplefile_content" {
+  value = local_file.samplefile.content
+}
+
+output "samplefile2_id" {
+  value = local_file.samplefile2.id
+}
+
+output "samplefile2_content" {
+  value = local_file.samplefile2.content
+}
