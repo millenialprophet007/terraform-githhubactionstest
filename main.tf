e3f8c67109646a97bb778a24075b71250b868e1d
millenialprophet007 <166893817+millenialprophet007@users.noreply.github.com>
2025-02-09 11:06:14 +0530

    Update main.tf with new file and output for samplefile

--- main.tf
+++ main.tf
@@ -2,3 +2,13 @@ resource "local_file" "samplefile" {
   filename = "samplefile.txt"
   content = "hello world"
 }
+
+
+resource "local_file" "samplefile2"
+  filename = "samplefile2.txt"
+  content = "This is the second file created"
+}
+
+output "samplefile_content" {
+  value = local_file.samplefile.content
+}
