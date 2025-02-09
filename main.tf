5c0dc6805c650d3cfed75b94160c71b0652e6fde
millenialprophet007 <166893817+millenialprophet007@users.noreply.github.com>
2025-02-09 11:08:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,12 +3,11 @@ resource "local_file" "samplefile" {
   content = "hello world"
 }
 
-
-resource "local_file" "samplefile2"
-  filename = "samplefile2.txt"
-  content = "This is the second file created"
-}
-
 output "samplefile_content" {
   value = local_file.samplefile.content
+}
+
+resource "local_file" "samplefile2" {
+  filename = "samplefile2.txt"
+  content = "This is the second sample file created"
 }
